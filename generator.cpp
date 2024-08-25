1b5fc0881bc9369ba1d85bd545d50e08f5350c1e
DarshJainme <148175920+DarshJainme@users.noreply.github.com>
2024-08-25 11:11:36 +0530

    Create generator.cpp

---
+++ generator.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    // reverse sorted
+    
+    freopen("input1.txt","w",stdout);
+    cout<<10000 <<endl;
+    for (int i = 0; i < 10000; i++)
+    {
+        cout<<10000-i<<' ';
+    }cout<<endl;
+    cout<<10000<<endl;
+    for (int i = 0; i < 10000; i++)
+    {
+        cout<<i+1<<' ';
+    }
+    cout<<endl;
+    srand(time(NULL));
+    cout<<10000<<endl;
+    for (int i = 0; i < 10000; i++)
+    {
+        cout<<rand()<<' ';
+    }
+    
+    cout<<endl;
+
+    
+    
+
+return 0;
+}
